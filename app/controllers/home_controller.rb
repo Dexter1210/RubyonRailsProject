03896cd8abae88080269490fb53205efe0e38fcd
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-11 12:47:03 +0530

    Inclusion of HTML page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
+    def index
+
+        #try to search for views/controllername/index.html.erb
+       # render plain: "This is index page"
+
+    end
+
     def ok
 
         #try to search for views/controllername/index.html.erb
-        render plain: "OK"
+        render plain: "ok"
 
     end
+
 
 end
 
