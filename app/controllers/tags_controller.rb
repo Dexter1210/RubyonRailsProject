4dbe8286cbd2a6f10fe0c777ade4fa0c8efe998f
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-11 17:33:18 +0530

    Inclusion of show and destroy method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,15 @@ class TagsController < ApplicationController
         @tags=Tag.all
     end
 
+    def show
+        @tag=Tag.find(params[:id])
+    end
+
+    def destroy
+        @tag=Tag.find(params[:id])
+        @tag.destroy
+        redirect_to tags_path
+    end
+
+
 end
